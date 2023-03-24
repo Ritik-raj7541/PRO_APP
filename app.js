6adf72dd2feac7f0fe1f481c636ce859a0cb7fef
Ritik <ritikrajbhagat1@gmail.com>
2023-03-24 11:24:19 +0530

    added post in user page and space for unauthorized user with wrong login credential

--- app.js
+++ app.js
@@ -2,40 +2,36 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const _ = require('lodash');
-const User = require('./modals/User') ;
-const Post = require('./modals/Post') ;
-const session = require('express-session') ;
-const passport = require('passport') ;
+const _ = require("lodash");
+const User = require("./modals/User");
+const Post = require("./modals/Post");
+const session = require("express-session");
+const passport = require("passport");
 
 //for dates
-const dateTime = require('node-datetime');
-
+const dateTime = require("node-datetime");
 
 const app = express();
-
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.use(session({
-  secret:"key that will sign cookie", 
-  resave: false,
-  saveUninitialized: false,
-  
-})) ;
+app.use(
+  session({
+    secret: "key that will sign cookie",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
-app.use(passport.initialize()) ;
-app.use(passport.session()) ;
-
+app.use(passport.initialize());
+app.use(passport.session());
 
 //configure session middleware
 
 mongoose.set("strictQuery", false);
 mongoose.connect("mongodb://127.0.0.1:27017/proDB");
-
-
 
 passport.use(User.createStrategy());
 
@@ -47,250 +43,281 @@ app.get("/", function (req, res) {
   res.render("home");
 });
 app.get("/login", function (req, res) {
-  res.render("login" );
+  res.render("login");
 });
 app.get("/register", function (req, res) {
   res.render("register");
 });
 app.get("/main", function (req, res) {
-  console.log('we are in main')
-  if(req.isAuthenticated()){
-    
-    const curUser = req.user.nickname ;
-    Post.find({}).sort({dateCreated: -1}).exec((err, docs) => { 
-      if(err){
-        console.log(err) ;
-      }else{
-        if(docs){
-          console.log('found') ;
-          console.log(docs) ;
-          res.render("main",{postMaterial:docs, user:curUser}) ;
-        }else{
-          console.log('not found') ;
-          console.log(docs) ;
-          res.render("main", {postMaterial:[], user:curUser}) ;
+  console.log("we are in main");
+  if (req.isAuthenticated()) {
+    const curUser = req.user.nickname;
+    Post.find({})
+      .sort({ dateCreated: -1 })
+      .exec((err, docs) => {
+        if (err) {
+          console.log(err);
+        } else {
+          if (docs) {
+            res.render("main", { postMaterial: docs, user: curUser });
+          } else {
+            res.render("main", { postMaterial: [], user: curUser });
+          }
         }
-      }
-     });
-   
-  }else{
-    res.redirect('/login') ;
+      });
+  } else {
+    res.redirect("/login");
   }
-
 });
 app.get("/compose", function (req, res) {
-  if(req.isAuthenticated()){
+  if (req.isAuthenticated()) {
     res.render("compose");
-  }else{
-    res.redirect('/login') ;
+  } else {
+    res.redirect("/login");
   }
-  
 });
 
-app.get('/post/:postName', function(req, res){
-  var id = req.params.postName ;
-  
-    Post.findById(id, function (err, foundPost) {
-    if (err){
-        console.log(err);
-    }
-    else{
-      res.render('post', {post:foundPost}) ;
-    }
-   });
-  
-}) ;
+app.get("/post/:postName", function (req, res) {
+  var id = req.params.postName;
 
-app.get('/user/:userId', function(req, res){
-   var searchedUser = req.params.userId ;
-   console.log(searchedUser) ;
-   User.findById(searchedUser, function(err, foundUser){
-    if(err){
-      console.log('err') ;
-    }else{
-      // console.log(foundUser) ;
-       res.render('user', {user:foundUser}) ;
+  Post.findById(id, function (err, foundPost) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("post", { post: foundPost });
     }
-   }) ;
-}) ;
+  });
+});
 
-app.get('/comment/:commentId', function(req, res){
-   console.log(req.params.commentId) ;
-   currentPostid = req.params.commentId ;
-    Post.findById(currentPostid, function(err, foundPost){
-    if(err){
-      console.log(err) ;
-    }else{
-      res.render('comment', {id:currentPostid, commentPost:foundPost.comment}) ;
+app.get("/user/:userId", async function (req, res) {
+  var searchedUser = req.params.userId;
+
+  let idAndName = [];
+  const foundUser = await User.findById(searchedUser);
+  for (const element of foundUser.post) {
+    const docs = await Post.findById(element);
+    if (docs) {
+      idAndName.push({
+        id: element,
+        name: docs.title,
+      });
     }
-    
-   })
-  
-}) ;
+  }
+  res.render("user", { user: foundUser, posts: idAndName });
+});
+
+app.get("/comment/:commentId", function (req, res) {
+  console.log(req.params.commentId);
+  currentPostid = req.params.commentId;
+  Post.findById(currentPostid, function (err, foundPost) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("comment", {
+        id: currentPostid,
+        commentPost: foundPost.comment,
+      });
+    }
+  });
+});
 
 //post request
 
-app.post("/main", function (req, res) {
+app.post("/main", function (req, res) {});
 
+app.post("/like", function (req, res) {
+  const curUserId = req.user.id;
+  const curPostId = req.body.liked;
+  console.log(curPostId);
+
+  Post.findById(curPostId, function (err, docs) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (docs) {
+        if (docs.dislike.includes(curUserId)) {
+          Post.findByIdAndUpdate(
+            curPostId,
+            { $pull: { dislike: curUserId } },
+            { new: true },
+            function (err, result) {
+              if (err) {
+                console.log(err);
+              } else {
+                console.log("success");
+              }
+            }
+          );
+        }
+        if (!docs.like.includes(curUserId)) {
+          Post.findByIdAndUpdate(
+            curPostId,
+            { $push: { like: curUserId } },
+            { new: true },
+            function (err, result) {
+              if (err) {
+                console.log(err);
+              } else {
+                res.redirect("/main");
+              }
+            }
+          );
+        } else {
+          res.redirect("/main");
+        }
+      }
+    }
+  });
+});
+app.post("/dislike", function (req, res) {
+  const curUserId = req.user.id;
+  const curPostId = req.body.disliked;
+  console.log(curPostId);
+
+  Post.findById(curPostId, function (err, docs) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (docs) {
+        if (docs.like.includes(curUserId)) {
+          Post.findByIdAndUpdate(
+            curPostId,
+            { $pull: { like: curUserId } },
+            { new: true },
+            function (err, result) {
+              if (err) {
+                console.log(err);
+              } else {
+                console.log("sucess");
+              }
+            }
+          );
+        }
+        if (!docs.dislike.includes(curUserId)) {
+          Post.findByIdAndUpdate(
+            curPostId,
+            { $push: { dislike: curUserId } },
+            { new: true },
+            function (err, result) {
+              if (err) {
+                console.log(err);
+              } else {
+                res.redirect("/main");
+              }
+            }
+          );
+        } else {
+          res.redirect("/main");
+        }
+      }
+    }
+  });
 });
 
-app.post('/like', function(req, res){
-   const curUserId = req.user.id ;
-   const curPostId = req.body.liked ;
-   console.log(curPostId) ;
-
-  Post.findById(curPostId, function(err, docs){
-    if(err){
-      console.log(err) ;
-    }
-    else{
-      if(docs){
-        if(docs.dislike.includes(curUserId)){
-         Post.findByIdAndUpdate(curPostId,{$pull:{dislike:curUserId}},{new:true}, function(err, result){
-           if(err){
-             console.log(err) ;
-           }else{
-             console.log('success') ;
-           }
-          }) ;
-        }
-        if( !docs.like.includes(curUserId) ){
-         Post.findByIdAndUpdate(curPostId,{$push:{like:curUserId}},{new:true}, function(err, result){
-           if(err){
-             console.log(err) ;
-           }else{
-             res.redirect('/main') ;
-           }
-          }) ;
-        }else{
-          res.redirect('/main') ;
-        }
-       }
-    }
-  })
-}) ;
-app.post('/dislike', function(req, res){
-   const curUserId = req.user.id ;
-   const curPostId = req.body.disliked ;
-   console.log(curPostId) ;
-
-  Post.findById(curPostId, function(err, docs){
-   if(err){
-    console.log(err) ;
-   }else{
-    if(docs){
-      if(docs.like.includes(curUserId)){
-        Post.findByIdAndUpdate(curPostId,{$pull:{like:curUserId}},{new:true}, function(err, result){
-          if(err){
-            console.log(err) ;
-          }else{
-            console.log('sucess') ;
-          }
-         }) ;
-      }
-      if(!docs.dislike.includes(curUserId)){
-        Post.findByIdAndUpdate(curPostId,{$push:{dislike:curUserId}},{new:true}, function(err, result){
-          if(err){
-            console.log(err) ;
-          }else{
-            res.redirect('/main') ;
-          }
-         }) ;
-      }else{
-        res.redirect('/main') ;
-      }
-    }
-   }  
-  })
-}) ;
-
-app.post("/login", function(req, res) {
+app.post("/login", function (req, res) {
   const user = new User({
-    username: req.body.username, 
-    password: req.body.password
-  }) ;
-  req.login(user, function(err){
-    if(err){
-      console.log(err) ;
-    }else{
-      passport.authenticate('local')(req, res, function(){
-        res.redirect('/main') ;
-      })
-    }
-  })
-  
+    username: req.body.username,
+    password: req.body.password,
+  });
+  passport.authenticate('local', function(err, user, info) {
 
+    if(err) { res.redirect('/login'); }
+    if(user){
+        req.logIn(user, function(err) {
+            if (err) { res.redirect('/login'); }
+            else res.redirect('/main');
+        });
+    }
+    else{ 
+        return res.redirect('/login'); 
+    }
+    
+})(req, res);
 });
 app.post("/register", async function (req, res) {
-  User.register({username: req.body.username ,fullname:req.body.fullname ,nickname: req.body.nickname , bio: req.body.bio}, req.body.password, function(err, user){
-    if(err){
-      console.log('we have a error') ;
-      console.log(err) ;
-      res.redirect('/register') ;
-    } else{
-      passport.authenticate('local')(req, res, function(){
-        res.redirect('/main') ;
-      })
+  User.register(
+    {
+      username: req.body.username,
+      fullname: req.body.fullname,
+      nickname: req.body.nickname,
+      bio: req.body.bio,
+    },
+    req.body.password,
+    function (err, user) {
+      if (err) {
+        console.log("we have a error");
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        passport.authenticate("local")(req, res, function () {
+          res.redirect("/main");
+        });
+      }
     }
-  })
+  );
 });
-
 
 app.post("/compose", function (req, res) {
   const heading = req.body.heading;
   const content = req.body.content;
-  const curUserId = req.user.id ;
-  const curUserNname = req.user.nickname ;
+  const curUserId = req.user.id;
+  const curUserNname = req.user.nickname;
   const dt = dateTime.create();
-  const today = dt.format('Y-m-d H:M:S');
+  const today = dt.format("Y-m-d H:M:S");
   const newUser = new Post({
     title: heading,
     post: content,
     name: curUserNname,
     UserId: curUserId,
-    dateCreated: today
-  }) ;
-  newUser.save(function(err, result){
-    if(err){
-      console.log(err) ;
-    }else{
-      console.log('post added successfully') ;
-      const curPostId = result.id ;
-      User.findByIdAndUpdate(curUserId, {$push:{post:curPostId}} , function(err){
-        if(err){
-          console.log(err) ;
-        }else{
-          res.redirect('/main') ;
+    dateCreated: today,
+  });
+  newUser.save(function (err, result) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("post added successfully");
+      const curPostId = result.id;
+      User.findByIdAndUpdate(
+        curUserId,
+        { $push: { post: curPostId } },
+        function (err) {
+          if (err) {
+            console.log(err);
+          } else {
+            res.redirect("/main");
+          }
         }
-      }) ;
+      );
     }
   });
-  
 
   // User.findByIdAndUpdate(curUser, {$push:{post:{}}})
 });
 
-app.post('/comment/:currentId', function(req, res){
-    const commentbyUser = req.body.comment ;
-     Post.findById(currentPostid, function(err, foundPost){
-      if(err){
-        console.log(err) ;
-      }else{
-        foundPost.comment.push({userName:req.user.nickname, essay:commentbyUser}) ;
-        foundPost.save() ;
-        res.redirect('/main') ;
-      }
-    }) ;
-}) ;
-
-app.post('/logout', function(req, res, next){
-  req.logout(function(err) {
-    if (err) { return next(err); }
-    res.redirect('/');
+app.post("/comment/:currentId", function (req, res) {
+  const commentbyUser = req.body.comment;
+  Post.findById(currentPostid, function (err, foundPost) {
+    if (err) {
+      console.log(err);
+    } else {
+      foundPost.comment.push({
+        userName: req.user.nickname,
+        essay: commentbyUser,
+      });
+      foundPost.save();
+      res.redirect("/main");
+    }
   });
 });
 
-
+app.post("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
 
 app.listen(3000, function () {
   console.log("site is running properly");
